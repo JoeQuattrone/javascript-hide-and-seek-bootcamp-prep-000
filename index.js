2ea3b5f7c5adb55dfc63f3edce259f482c6dd334
Joe Quattrone <joequattrone5@gmail.com>
2018-10-09 13:51:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function nestedTarget() {
  function deepestChild() {
   var search = document.getElementById('grand-node').querySelectorAll('div'); 
  
-  for (let i = 0; i < search.length - 1; i++) {
-    if (search[i].innerHTML == "boo!")  {
+  for (let i = 0; i < search.length; i++) {
+    if (search[i] == search.length - 1)  {
       return search
     }
   } 
