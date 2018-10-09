29827570750e0dc054e65002a777fb6604520dcf
Joe Quattrone <joequattrone5@gmail.com>
2018-10-09 13:30:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('div.target')
+}
+
+
+ function increaseRankBy(n) {
+  var listItems =  document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < listItems.length; i++) {
+
+     listItems[i].innerHTML = parseInt(listItems[i].innerHTML) + n;
+  }
+} 
+
+ function deepestChild() {
+  var search = document.getElementById('grand-node').querySelectorAll('div'); 
+ 
+  for (let i = 0; i < search.length; i++) {
+    if (search[i].innerHTML == "boo!")  {
+      return search[i].innerHTML
+    }
+  } 
+} 
+
+/* function deepestChild() {
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for(let i=0;i<lis.length - 1;i++) {
+    test=lis[i].querySelector("div");
+  }
+return test;
+} */
+
 
